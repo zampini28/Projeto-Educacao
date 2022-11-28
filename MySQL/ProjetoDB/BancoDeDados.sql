7e6e52ed016aaa647d5aa31eca3ce12a82360a25
zampini28 <caio.zampini@gmail.com>
2022-11-28 06:06:30 -0300

    MySQL update

---
+++ MySQL/ProjetoDB/BancoDeDados.sql
@@ -0,0 +1,57 @@
+#--------------------------
+# Start setup
+#--------------------------
+DROP SCHEMA IF EXISTS testdb;
+CREATE SCHEMA IF NOT EXISTS testdb;
+USE testdb;
+
+#--------------------------
+# Creating tables
+#--------------------------
+CREATE TABLE IF NOT EXISTS testdb.Usuario (
+    id          INT             NOT NULL    AUTO_INCREMENT,
+    nome        VARCHAR(255)    NOT NULL,
+    rg          VARCHAR(255)    NOT NULL,
+    cpf         VARCHAR(255)    NOT NULL,
+    n_telefone  VARCHAR(255)    NULL        DEFAULT NULL,
+    email       VARCHAR(255)    NOT NULL,
+    usuario     VARCHAR(255)    NOT NULL,
+    nascimento  DATETIME        NOT NULL,
+    cadastro    DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP(),
+    bloqueado   TINYINT         NOT NULL    DEFAULT 0,
+    senha       INT             NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS testdb.Professor(
+    id              INT NOT NULL    AUTO_INCREMENT,
+    disciplina_id   INT NOT NULL,
+    usuario_id      INT NOT NULL    AUTO_INCREMENT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS testdb.Administrador (
+    id          INT NOT NULL    AUTO_INCREMENT,
+    usuario_id  INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS testdb.Responsavel (
+    id              INT             NOT NULL    AUTO_INCREMENT,
+    cadastrador_id  INT             NOT NULL,
+    usuario_id      INT             NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS testdb.Aluno (
+    id              INT             NOT NULL    AUTO_INCREMENT,
+    matricula       VARCHAR(255)    NOT NULL    UNIQUE,
+    cadastrador_id  INT             NOT NULL,
+    usuario_id      INT             NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS testdb.Turma (
+    id INT NOT NULL AUTO_INCREMENT,
+    
+);
---
+++ main.rs
@@ -0,0 +1,8 @@
+fn main()
+{
+    let x: i32;
+    
+    x = 14;
+    
+    println!("{x}");
+}
