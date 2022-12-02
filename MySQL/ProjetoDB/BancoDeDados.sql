b02076b93f7823956f53fa5b9aaa50846287c70f
zampini28 <caio.zampini@gmail.com>
2022-12-02 19:43:27 -0300

    MySQL update

--- MySQL/ProjetoDB/BancoDeDados.sql
+++ MySQL/ProjetoDB/BancoDeDados.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS testdb.Usuario (
     cpf         VARCHAR(255)    NOT NULL,
     n_telefone  VARCHAR(255)    NULL        DEFAULT NULL,
     email       VARCHAR(255)    NOT NULL,
-    usuario     VARCHAR(255)    NOT NULL,
+    usuario     VARCHAR(255)    NOT NULL    UNIQUE,
     nascimento  DATE            NOT NULL,
     cadastro    DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP(),
     bloqueado   TINYINT         NOT NULL    DEFAULT 0,
@@ -241,64 +241,129 @@ ALTER TABLE testdb.Aluno_Responsavel
         REFERENCES testdb.Aluno (id);
 
 #--------------------------
+# Stored Procedures
+#--------------------------
+DELIMITER $$
+CREATE PROCEDURE adicionar_administrador (
+ IN nome_ VARCHAR(255), IN rg_ VARCHAR(255), IN cpf_ VARCHAR(255),
+ IN n_telefone_ VARCHAR(255), IN email_ VARCHAR(255), IN usuario_ VARCHAR(255),
+ IN nascimento_ VARCHAR(255), IN senha_ VARCHAR(255)
+ )
+BEGIN
+    INSERT INTO testdb.Usuario VALUES 
+    (DEFAULT, nome_, rg_, cpf_, n_telefone_, email_, usuario_, nascimento_, DEFAULT, DEFAULT, senha_);
+    INSERT INTO testdb.Administrador VALUES
+    (DEFAULT, (SELECT id FROM testdb.Usuario WHERE usuario=usuario_));
+END $$
+
+CREATE PROCEDURE adicionar_professor (
+ IN nome_ VARCHAR(255), IN rg_ VARCHAR(255), IN cpf_ VARCHAR(255),
+ IN n_telefone_ VARCHAR(255), IN email_ VARCHAR(255), IN usuario_ VARCHAR(255),
+ IN nascimento_ VARCHAR(255), IN senha_ VARCHAR(255), IN disciplina_ VARCHAR(255))
+BEGIN
+    INSERT INTO testdb.Usuario VALUES 
+    (DEFAULT, nome_, rg_, cpf_, n_telefone_, email_, usuario_, nascimento_, DEFAULT, DEFAULT, senha_);
+    INSERT INTO testdb.Professor VALUES
+    (DEFAULT, (SELECT id FROM testdb.Disciplina WHERE disciplina=disciplina_), 
+    (SELECT id FROM testdb.Usuario WHERE usuario=usuario_));
+END $$
+
+CREATE PROCEDURE adicionar_responsavel (
+ IN nome_ VARCHAR(255), IN rg_ VARCHAR(255), IN cpf_ VARCHAR(255),
+ IN n_telefone_ VARCHAR(255), IN email_ VARCHAR(255), IN usuario_ VARCHAR(255),
+ IN nascimento_ VARCHAR(255), IN senha_ VARCHAR(255), IN cadastrador INT)
+BEGIN
+    INSERT INTO testdb.Usuario VALUES 
+    (DEFAULT, nome_, rg_, cpf_, n_telefone_, email_, usuario_, nascimento_, DEFAULT, DEFAULT, senha_);
+    INSERT INTO testdb.Responsavel VALUES
+    (DEFAULT, cadastrador, (SELECT id FROM testdb.Usuario WHERE usuario=usuario_));
+END $$
+
+CREATE PROCEDURE adicionar_aluno (
+ IN nome_ VARCHAR(255), IN rg_ VARCHAR(255), IN cpf_ VARCHAR(255),
+ IN n_telefone_ VARCHAR(255), IN email_ VARCHAR(255), IN usuario_ VARCHAR(255),
+ IN nascimento_ VARCHAR(255), IN senha_ VARCHAR(255), IN matricula_ VARCHAR(255), 
+ IN cadastrador_id_ INT)
+BEGIN
+    INSERT INTO testdb.Usuario VALUES 
+    (DEFAULT, nome_, rg_, cpf_, n_telefone_, email_, usuario_, nascimento_, DEFAULT, DEFAULT, senha_);
+    INSERT INTO testdb.Aluno VALUES
+    (DEFAULT, matricula_, cadastrador_id_, (SELECT id FROM testdb.Usuario WHERE usuario=usuario_));
+END $$
+DELIMITER ;
+
+#--------------------------
 # Inserting new records
 #--------------------------
-INSERT INTO testdb.Usuario 
-(nome, rg, cpf, n_telefone, email, usuario, nascimento, senha)
+SET @id_do_cadastrador = 1;
+
+INSERT INTO testdb.Disciplina 
+ (disciplina)
 VALUES
-('Luana Rocha Sousa', '45.560.970-9', '551.296.556-56', 
- '(11) 96845-6020', 'LuanaRochaSousa@gmail.com', 
- 'LuanaRocha', '2006-06-12', 'ahShoop7ath'),
-
-('Melissa Ferreira Barbosa', '53.459.957-9', '162.146.465-27', 
- '(11) 97058-6574', 'MelissaFerreiraBarbosa@gmail.com', 
- 'MFerreira', '2006-09-13', 'Piej8eeYi'),
-
-('Luis Silva Pinto', '52.606.404-2', '283.707.729-19', 
- '(11) 92215-2205', 'LuisSilvaPinto@gmail.com', 
- 'LuizSPinto', '2006-10-22', 'cah9tah2Ei'),
-
-('Carla Correia Rocha', '55.422.483-9', '998.224.670-44', 
- '(11) 98255-7689', 'CarlaCorreiaRocha@gmail.com', 
- 'CCrocha', '2006-12-29', 'ahsheiG4oogh'),
-
-('Breno Fernandes Rodrigues', '45.392.117-4', '507.346.788-43', 
- '(11) 94690-3040', 'BrenoFernandesRodrigues@gmail.com', 
- 'Breno_Fernandes', '1951-07-30', 'iqu2dohPhai'),
-
-('Daniel Araujo Carvalho', '45.392.179-3', '269.857.143-82', 
- '(11) 95062-5064', 'DanielAraujoCarvalho@gmail.com', 
- 'DanielAraujo_Carvalho', '1989-04-19', 'Biezae4aer'),
-
-('Mariana Santos Rocha', '49.298.328-8', '459.842.703-58', 
- '(11) 92981-3140', 'MarianaSantosRocha@gmail.com', 
- 'Mariana.S.Rocha', '1977-01-21', 'Neip0unai0'),
-
-('Júlio Costa Souza', '47.166.673-3', '404.836.087-69', 
- '(11) 97527-7296', 'JulioCostaSouza@gmail.com', 
- 'JúlioCosta', '1991-04-28', 'oov7fu8I'),
-
-('Eduarda Pereira Oliveira', '54.820.520-4', '328.620.228-29', 
- '(11) 93283-3603', 'EduardaPereiraOliveira@gmail.com', 
- 'EduardaPOliveira', '1998-03-30', 'Quoor5OoGh2'),
-
-('Fernanda Araujo Souza', '49.296.479-8', '645.170.901-83', 
+ ('Língua Portuguesa'), ('Matemática'), ('História'), ('Geografia'),
+ ('Ciências'), ('Artes'), ('Educação Física'), ('Inglês');
+ 
+CALL testdb.adicionar_administrador (
+ 'Fernanda Araujo Souza', '49.296.479-8', '645.170.901-83', 
  '(11) 99681-2557', 'FernandaAraujoSouza@gmail.com', 
  'FernandaSouza', '1996-01-31', 'eeFee6ohl');
 
-INSERT INTO testdb.Administrador (usuario_id) VALUES (10);
+CALL testdb.adicionar_professor (
+ 'Júlio Costa Souza', '47.166.673-3', '404.836.087-69', 
+ '(11) 97527-7296', 'JulioCostaSouza@gmail.com', 
+ 'JúlioCosta', '1991-04-28', 'oov7fu8I', 'Matemática');
 
-INSERT INTO testdb.Professor (usuario_id) VALUES (8), (9);
+CALL testdb.adicionar_professor (
+ 'Eduarda Pereira Oliveira', '54.820.520-4', '328.620.228-29', 
+ '(11) 93283-3603', 'EduardaPereiraOliveira@gmail.com', 
+ 'EduardaPOliveira', '1998-03-30', 'Quoor5OoGh2', 'Língua Portuguesa');
+ 
+CALL testdb.adicionar_responsavel (
+ 'Breno Fernandes Rodrigues', '45.392.117-4', '507.346.788-43', 
+ '(11) 94690-3040', 'BrenoFernandesRodrigues@gmail.com', 
+ 'Breno_Fernandes', '1951-07-30', 'iqu2dohPhai', @id_do_cadastrador);
 
-INSERT INTO testdb.Aluno
-(matricula, cadastrador_id, usuario_id)
-VALUES
-('P2DSr2NFsDwzKqikF084vI9OddcID0e', 1, 1),
-('NTukjJ7bL7IV5AnVnjt8xuzCXkFyHhI', 1, 2),
-('NFLPnaQjxbTcgIcODV7IuDsGdu2HvhK', 1, 3),
-('kRGiMQBr3NmCdMlSWxAI0zaE7UivYkC', 1, 4);
+CALL testdb.adicionar_responsavel (
+ 'Daniel Araujo Carvalho', '45.392.179-3', '269.857.143-82', 
+ '(11) 95062-5064', 'DanielAraujoCarvalho@gmail.com', 
+ 'DanielAraujo_Carvalho', '1989-04-19', 'Biezae4aer', @id_do_cadastrador);
 
-INSERT INTO testdb.Responsavel
-(cadastrador_id, usuario_id)
-VALUES
-(1, 5), (1, 6), (1, 7);
+CALL testdb.adicionar_responsavel (
+ 'Mariana Santos Rocha', '49.298.328-8', '459.842.703-58', 
+ '(11) 92981-3140', 'MarianaSantosRocha@gmail.com', 
+ 'Mariana.S.Rocha', '1977-01-21', 'Neip0unai0', @id_do_cadastrador);
+ 
+CALL testdb.adicionar_aluno (
+ 'Luana Rocha Sousa', '45.560.970-9', '551.296.556-56', 
+ '(11) 96845-6020', 'LuanaRochaSousa@gmail.com', 
+ 'LuanaRocha', '2006-06-12', 'ahShoop7ath', '2009201003009-8', @id_do_cadastrador);
+
+CALL testdb.adicionar_aluno ('
+ Melissa Ferreira Barbosa', '53.459.957-9', '162.146.465-27', 
+ '(11) 97058-6574', 'MelissaFerreiraBarbosa@gmail.com', 
+ 'MFerreira', '2006-09-13', 'Piej8eeYi', '2529701099009-4', @id_do_cadastrador);
+ 
+CALL testdb.adicionar_aluno (
+ 'Luis Silva Pinto', '52.606.404-2', '283.707.729-19', 
+ '(11) 92215-2205', 'LuisSilvaPinto@gmail.com', 
+ 'LuizSPinto', '2006-10-22', 'cah9tah2Ei', '5821201033009-6', @id_do_cadastrador);
+
+CALL testdb.adicionar_aluno (
+ 'Carla Correia Rocha', '55.422.483-9', '998.224.670-44', 
+ '(11) 98255-7689', 'CarlaCorreiaRocha@gmail.com', 
+ 'CCrocha', '2006-12-29', 'ahsheiG4oogh', '9425501033029-5', @id_do_cadastrador);
+ 
+UPDATE testdb.Professor
+SET disciplina_id = (SELECT id FROM testdb.Disciplina WHERE disciplina='Ciências')
+WHERE usuario_id=(SELECT id FROM testdb.Usuario WHERE usuario='JúlioCosta');
+
+CALL testdb.adicionar_administrador (
+ 'Nicole Santos Castro', '54.004.755-5', '955.915.067-71',
+ '(11) 96512-7796', 'NicoleSantosCastro@gmail.com',
+ 'NicoleCastro', '1957-07-08', 'No4nee6zaigh');
+
+
+DELETE FROM testdb.Administrador WHERE usuario_id=
+ (SELECT id FROM testdb.Usuario WHERE usuario='NicoleCastro');
+
+DELETE FROM testdb.Usuario WHERE usuario='NicoleCastro';
